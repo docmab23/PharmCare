5c3e69fe7129a9b2a58a6fe08a8686e2c710ac3e
docmab23 <docmab23@gmail.com>
2022-11-25 00:50:54 -0500

    tabix added and files removed

--- run_pypgx.py
+++ run_pypgx.py
@@ -20,9 +20,35 @@ parser.add_argument('-d', '--drug' , required=True)
 #genes =['cacna1s', 'cftr', 'cyp1a1', 'cyp1a2', 'cyp1b1', 'cyp2a6', 'cyp2a13', 'cyp2b6', 'cyp2c8', 'cyp2c9', 'cyp2c19', 'cyp2d6', 'cyp2e1', 'cyp2f1', 'cyp2j2', 'cyp2r1', 'cyp2s1', 'cyp2w1', 'cyp3a4', 'cyp3a5', 'cyp3a7', 'cyp3a43']
 args = parser.parse_args()
 
+
 drug = args.drug 
 vcf = args.vcf
 id = args.jobid
+
+if vcf.endswith(".gz"):
+    print("zipped file ")
+    if not os.path.exits(str(vcf)+".tbi"):
+        try:
+            os.system(("tabix -p vcf {vcf}".format(vcf=vcf)))
+        except:
+            print(vcf.split(".gz")[0])
+            os.system("gunzip {vcf}".format(vcf=vcf)))
+            os.system("bgzip -c {vcf_un} > {vcf2}".format(vcf_un=vcf.split(".gz")[0] , vcf2 = vcf))
+            os.system("tabix -p vcf {vcf}".format(vcf=vcf))
+
+        
+
+else:
+     print(vcf+".gz")
+     print("Zipping using bgzip")
+     os.system("bgzip -c {vcf} > {vcf2}".format(vcf=vcf,vcf2=vcf +".gz"))
+     os.system("tabix -p vcf {vcf2}".format(vcf2 = vcf+".gz"))
+    # print("index made")
+
+     vcf = str(vcf)+".gz"
+
+
+
 
 focus_df = df.loc[(df['Drug'] == str(drug).lower())]
 
@@ -63,20 +89,15 @@ if len(unqiue_genes)==1:
 
     print(recommendation)
 
+    os.system("rm -r {vcf}".format(vcf=vcf))
+    os.system("rm -r *.tbi")
+    os.system("rm -r {dir}".format(dir=id))
 
 
 
 
 
-
-
-
-
-
-
-    
-
-
+""""
 #print(groups)
 
 #print(pypgx.predict_phenotype('CFTR', '*1', '*5'))
@@ -88,3 +109,4 @@ if len(unqiue_genes)==1:
 #pypgx.api.pipeline.run_ngs_pipeline("CYP1A1" , "new_fi", variants="test.vcf.gz")
 #df = pypgx.load_cpic_table()
 #df.to_csv("utils/cpic_table.csv")
+"""
